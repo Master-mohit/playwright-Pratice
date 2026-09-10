b73682807976fd98845643ba48d48e4727a8fa60
Master-mohit <mohitmansigupta9098@gmail.com>
2026-09-10 21:04:28 +0530

    run last task for today

--- tests/third.spec.js
+++ tests/third.spec.js
@@ -31,3 +31,19 @@ test('trial', async({ page }) => {
     await page.locator('img').click();
     await page.getByRole('button', {name: 'Add to cart'}).click()
 
+
+    // ...............Task 4.................
+
+    test('Product Selection', async({page})=> {
+
+    await page.goto('https://www.saucedemo.com');
+    await page.getByLabel('Username').fill('standard_user');
+    await page.getByLabel('Password').fill('secret_sauce');
+    await page.getByRole('button', {name: 'Login'}).click();
+    await expect(page).toHaveURL('/inventory.html');
+    await expect(page.getByText('Sauce Labs Backpack')).toBeVisible();
+    await page.getByRole('button', {name: 'Add to cart'}).click();
+    await page.getByRole('link', { name: /shopping cart/i }).click();
+    await expect(page.getByText('Sauce Labs Backpack')).toBeVisible();
+
+    })
